1d43acef2ef7934d50c703a0d6be149fe00a4f7a
ottiy <au1234555666777@gmail.com>
2015-07-17 11:38:55 +0900

    眼科いらず改良

--- app/src/main/java/com/example/ochiai/careyoureye/OperatingTimeTestActivity.java
+++ app/src/main/java/com/example/ochiai/careyoureye/OperatingTimeTestActivity.java
@@ -1,29 +1,23 @@
 package com.example.ochiai.careyoureye;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class VisionTestStartActivity extends ActionBarActivity {
+public class OperatingTimeTestActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_vision_test_start);
+        setContentView(R.layout.activity_operating_time_test);
     }
 
-    public void start(View v){
-        Intent intent = new Intent(this,VisionTsetActivity.class);
-        startActivity(intent);
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_vision_test, menu);
+        getMenuInflater().inflate(R.menu.menu_timer_test, menu);
         return true;
     }
 
--- app/src/main/java/com/example/ochiai/careyoureye/VisionTsetActivity.java
+++ app/src/main/java/com/example/ochiai/careyoureye/VisionTsetActivity.java
@@ -3,12 +3,14 @@ package com.example.ochiai.careyoureye;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -17,10 +19,12 @@ public class VisionTsetActivity extends ActionBarActivity {
     TextView level;
     TextView trueNumText;
     TextView falseNumText;
+    ImageView randallRingImageView;
     int trueNum;
     int falseNum;
     double lingsSize;
     double levelNum;
+    int[] rundleRing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +32,14 @@ public class VisionTsetActivity extends ActionBarActivity {
         setContentView(R.layout.activity_vision_tset);
 
         level = (TextView)findViewById(R.id.level);
-        trueNumText=(TextView)findViewById(R.id.trueNumText);
-        falseNumText=(TextView)findViewById(R.id.falseNumText);
-        trueNum=0;
-        falseNum=0;
+        trueNumText = (TextView)findViewById(R.id.trueNumText);
+        falseNumText = (TextView)findViewById(R.id.falseNumText);
+        randallRingImageView= (ImageView)findViewById(R.id.randallRing);
+        trueNum = 0;
+        falseNum = 0;
+        levelNum = 0.1;
+        rundleRing = new int[5];
+
     }
 
     public void trueButton(View v){
@@ -41,22 +49,23 @@ public class VisionTsetActivity extends ActionBarActivity {
         if (trueNum>=2){
             levelNum=levelNum+0.1;
             lingsSize=7.5/levelNum;
-            randallRing.setLayoutParams(new FrameLayout.LayoutParams(lingsSize,lingsSize));
+            randallRingImageView.setLayoutParams(new FrameLayout.LayoutParams(lingsSize, lingsSize));
             trueNum=0;falseNum=0;//正解数と不正解数をリセット
         }
+        level.setText("視力"+levelNum);
         trueNumText.setText(trueNum+"回");
     }
 
     public void falseButton(View v){
         falseNum=falseNum+1;
 
-        if (falseNum>=2){
+        if (falseNum >= 2){
             //ダイアログボックスを表示
             AlertDialog.Builder builder =new AlertDialog.Builder(this);
             builder.setMessage("視力検査を終了しますか？");
             builder.show();
             builder.setTitle("あなたの視力は" + levelNum + "です。");
-            DialogInterface.OnClickListener listener =
+            DialogInterface.OnClickListener listener = 
                     new DialogInterface.OnClickListener(){
                         @Override
                     public void onClick(DialogInterface dialog, int which){
@@ -64,13 +73,14 @@ public class VisionTsetActivity extends ActionBarActivity {
                             switch (which){
                                 case DialogInterface.BUTTON_POSITIVE:
                                     msg ="もう一回する";
-                                    Intent intentPo = new Intent(com.example.ochiai.careyoureye., MainActivity.class);
-                                    startActivity(intentPo);
+                                    //Intent intentPo = new Intent(this..class);
+                                    //startActivity(intentPo);
                                     break;
                                 case DialogInterface.BUTTON_NEGATIVE:
                                     msg = "視力検査をやめる";
-                                    Intent intentNe =new Intent(this,VisionTsetActivity.class);
-                                    startActivity(intentNe);
+                                    //Intenthis.sionTsetActivity.class));
+                                    //startActivity(intentNe);
+                                    break;
 
                             }
                         }
